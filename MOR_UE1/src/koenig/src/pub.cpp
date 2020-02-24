61ecbfb9d8b2b06ef3956feef0b7fa5beeb798e7
holzpfanne <dummy@dummy.com>
2020-02-24 11:38:50 +0100

    added comments

--- MOR_UE1/src/koenig/src/pub.cpp
+++ MOR_UE1/src/koenig/src/pub.cpp
@@ -7,13 +7,13 @@ int main( int argc, char** argv ) { // Note, Wen need argc and argv for node ini
     ros::init( argc, argv, "Publisher" ); // Init Node with given parameters (e.g.: arguments)
     ros::NodeHandle n( "~" ); // The node handle is used for node startup/shutdown and namespace
                               // handling (note: ~ => private node namespace)
-    //----------------------//
-    //--- Create a topic ---//
-    //----------------------//
+
     ros::Publisher chatter_pub = n.advertise< koenig::custom >(
-        "mytopicPub", 1 );    // Create a topic with message=string, the bufferlength is 1
+                                    "mytopicPub", 1 );    // Create a topic with message=custom msg, the bufferlength is 1
     
     ros::Rate loop_rate( 10 ); // Spin with this rate
+
+    //create and init msg data
     koenig::custom msg;
     msg.pos = 0.5;
     msg.radius = 5;
@@ -23,8 +23,8 @@ int main( int argc, char** argv ) { // Note, Wen need argc and argv for node ini
     while( ros::ok( ) ) {      // Do till ROS error occurs (e.g.: strg+C or ROS shutdown signal)
         chatter_pub.publish( msg );    // Send message
         ros::spinOnce( );              // Update buffer
-        loop_rate.sleep( ); // Sleep with given frequency
-        msg.radmove++;          //move robot
+        loop_rate.sleep( );            // Sleep with given frequency
+        msg.radmove++;                 //move robot
     }
     return 0; // Everything is fine :)
 }
--- MOR_UE1/src/koenig/src/sub.cpp
+++ MOR_UE1/src/koenig/src/sub.cpp
@@ -1,22 +1,20 @@
 #include "ros/ros.h"
 #include <math.h>
 #include "koenig/custom.h"     //Include custom msg
-//--------------------------------------//
-//--- Function attached to the topic ---//
-//--------------------------------------//
+
 void callback( const koenig::custom::ConstPtr& msg ) {         // Function is called when message arrived
     int X, Y;
     X = (msg->radmove * msg->radius) * cos(msg->pos);   //radiant * radius * cos(orientierung) 
-    Y = (msg->radmove * msg->radius) * sin(msg->pos);
+    Y = (msg->radmove * msg->radius) * sin(msg->pos);   //radiant * radius * sin(orientierung)
     ROS_INFO_STREAM( "X: " << X << "/Y: " << Y);
 }
 int main( int argc, char** argv ) {
-    ros::init( argc, argv, "Subscriber" ); // Init node with given parameters and node name
-    ros::NodeHandle n( "~" ); // The node handle is used for node startup/shutdown and namespace handling (note: ~ => private node namespace)
-    ros::Subscriber sub = n.subscribe( "mytopicSub", // Listen to this topic
+    ros::init( argc, argv, "Subscriber" );              // Init node with given parameters and node name
+    ros::NodeHandle n( "~" );                           // The node handle is used for node startup/shutdown and namespace handling (note: ~ => private node namespace)
+    ros::Subscriber sub = n.subscribe( "mytopicSub",    // Listen to this topic
                                        1,              // Buffer length is 1
                                        callback );     // Function to be called when message arrives
     ROS_INFO_STREAM( "Init Subscriber complete");                                  
     ros::spin( );                                      // Do a ''main-loop'', which waits for new messages
-    return 0;                                          // Everything is fine :)
+    return 0;
 }
