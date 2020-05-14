3316ce52683f2d06096c3b282db07ade8c3b250e
holzpfanne <dummy@dummy.com>
2020-05-14 21:05:42 +0200

    one working

--- MOR_UE3/src/koenig/src/main.cpp
+++ MOR_UE3/src/koenig/src/main.cpp
@@ -1,26 +1,27 @@
 #include <ros/ros.h>
 #include <iostream>
-#include <nav_msgs/OccupancyGrid.h>
+#include <move_base_msgs/MoveBaseActionGoal.h>
 
-nav_msgs::OccupancyGrid map;
-bool loaded = false;
+int main(int argc, char **argv) {
 
-void get_map(const nav_msgs::OccupancyGrid map_in){
-
-    map = map_in;
-    loaded = true;
-}
-
-
-int main(int argc, char** argv) {
     ros::init(argc, argv, "finder");
     ros::NodeHandle n("~koenig");
 
-    ros::Subscriber map_loader = n.subscribe<nav_msgs::OccupancyGrid>("OccupancyGrid", 1, get_map);
+    move_base_msgs::MoveBaseActionGoal goal;
+    goal.goal.target_pose.pose.position.x = 7.5;
+    goal.goal.target_pose.pose.position.y = 7.1;
+    goal.goal.target_pose.pose.orientation.w = 1;
+    goal.goal.target_pose.header.frame_id = "map";
 
+    ros::Publisher goal_pub = n.advertise<move_base_msgs::MoveBaseActionGoal>("goal", 1);
 
-    while(!loaded);
-
+    ros::Rate myclock(10);
+    
+    while(ros::ok()){
+        goal_pub.publish(goal);
+        ros::spinOnce();
+    }
+    
 
     return 0;
 }
